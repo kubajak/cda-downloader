06131a28936ce3569ed40af69cfc0a8f6c6f3183
kubajak <43299916+kubajak@users.noreply.github.com>
2023-05-07 21:15:08 +0200

    Update cda.py

--- cda.py
+++ cda.py
@@ -1,15 +1,28 @@
+import os
+import sys
 import requests
 from bs4 import BeautifulSoup
 import subprocess
+from datetime import datetime
 
-# funkcja do pobierania jednego linku przy użyciu yt-dlp
-def download_link(link):
-    subprocess.run(['yt-dlp.exe', 'https://www.cda.pl' + link])
+def download_link(link, folder_name):
+    # utworzenie nowego folderu, jeśli nie istnieje
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    # pobranie filmu i zapisanie go w nowym folderze
+    subprocess.run(['yt-dlp.exe', '-o', f'{folder_name}/%(title)s.%(ext)s', 'https://www.cda.pl' + link])
 
 if __name__ == '__main__':
     # pobranie linku do folderu od użytkownika
     user_link = str(input("Podaj link: "))
-    page = requests.get(user_link)
+    
+    try:
+        page = requests.get(user_link)
+    except requests.exceptions.RequestException as e:
+        print(f"Wystąpił błąd podczas pobierania strony: {e}")
+        sys.exit(1)
+
     soup = BeautifulSoup(page.content, 'html.parser')
 
     # znalezienie linków do filmów na stronie
@@ -19,6 +32,15 @@ if __name__ == '__main__':
         if href and href.startswith('/video/'):
             links.append(href)
 
+    # pobranie nazwy folderu od użytkownika
+    folder_name = str(input("Podaj nazwę folderu (lub zostaw puste dla domyślnej nazwy): ")).strip()
+
+    # użycie domyślnej nazwy folderu, jeśli użytkownik nie podał nazwy
+    if not folder_name:
+        now = datetime.now()
+        date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+        folder_name = f'pobrane_filmy_{date_time}'
+
     # pobranie każdego filmu po kolei
     for link in links:
-        download_link(link)
+        download_link(link, folder_name)
