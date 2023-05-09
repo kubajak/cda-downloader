4558999db140baf69b98b5403da76fbb0975a7ca
kubajak <43299916+kubajak@users.noreply.github.com>
2023-05-09 14:29:07 +0200

    Update cda.py

--- cda.py
+++ cda.py
@@ -6,64 +6,94 @@ import subprocess
 from datetime import datetime
 import threading
 
-# pobieranie yt-dlp jeśli nie zostanie znaleźony
-if not os.path.exists('yt-dlp.exe'):
-    print('Downloading yt-dlp...')
-    subprocess.run(['curl', '-L', 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe', '-o', 'yt-dlp.exe'])
+def pobierz_ytdlp():
+    """Pobierz yt-dlp jeśli nie zostanie znaleziony."""
+    if not os.path.exists('yt-dlp.exe'):
+        print('Pobieranie yt-dlp...')
+        try:
+            subprocess.run(['curl', '-L', 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe', '-o', 'yt-dlp.exe'])
+        except Exception as e:
+            print(f'Błąd podczas pobierania yt-dlp: {e}')
+            sys.exit(1)
 
-def download_link(link, folder_name):
-    # utworzenie nowego folderu, jeśli nie istnieje
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-
-    # pobranie filmu i zapisanie go w nowym folderze
+def utworz_katalog(nazwa_katalogu):
+    """Utwórz nowy katalog jeśli nie istnieje."""
     try:
-        subprocess.run(['yt-dlp.exe', '-o', f'{folder_name}/%(title)s.%(ext)s', 'https://www.cda.pl' + link])
+        if not os.path.exists(nazwa_katalogu):
+            os.makedirs(nazwa_katalogu)
+    except OSError as e:
+        print(f'Błąd podczas tworzenia katalogu: {e}')
+        return False
+    return True
+
+def pobierz_film(link, nazwa_katalogu):
+    """Pobierz film i zapisz go w nowym katalogu."""
+    try:
+        subprocess.run(['yt-dlp.exe', '-o', f'{nazwa_katalogu}/%(title)s.%(ext)s', 'https://www.cda.pl' + link])
     except Exception as e:
-        print(f'Error downloading video: {e}')
+        print(f'Błąd podczas pobierania filmu: {e}')
 
-if __name__ == '__main__':
-    # pobranie linku do folderu od użytkownika
-    user_link = str(input("Podaj link do folderu: "))
-        # sprawdzenie czy podany link pochodzi z cda.pl i ponowne pytanie o link jeśli nie jest z cda.pl
-    while not user_link.startswith('https://www.cda.pl'):
+def pobierz_link_uzytkownika():
+    """Pobierz link do katalogu od użytkownika."""
+    link_uzytkownika = str(input("Podaj link do katalogu: "))
+    # Sprawdź czy podany link pochodzi z cda.pl i ponownie pytaj o link jeśli nie jest z cda.pl
+    while not link_uzytkownika.startswith('https://www.cda.pl'):
         print("Podany link nie jest z cda.pl")
-        user_link = str(input("Podaj link do folderu: "))
+        link_uzytkownika = str(input("Podaj link do katalogu: "))
+    return link_uzytkownika
 
+def pobierz_zawartosc_strony(link_uzytkownika):
+    """Pobierz zawartość strony pod podanym linkiem."""
     try:
-        page = requests.get(user_link)
+        strona = requests.get(link_uzytkownika)
     except requests.exceptions.RequestException as e:
         print(f"Wystąpił błąd podczas pobierania strony: {e}")
         sys.exit(1)
+    return strona.content
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    # znalezienie linków do filmów na stronie
-    links = []
+def pobierz_linki_do_filmow(zawartosc_strony):
+    """Znajdź linki do filmów na stronie."""
+    soup = BeautifulSoup(zawartosc_strony, 'html.parser')
+    linki = []
     for link in soup.find_all('a'):
         href = link.get('href')
         if href and href.startswith('/video/'):
-            links.append(href)
+            linki.append(href)
+    # Usuń duplikaty z listy linków
+    linki = list(dict.fromkeys(linki))
+    return linki
 
-    # usunięcie duplikatów z listy links
-    links = list(dict.fromkeys(links))
+def pobierz_nazwe_katalogu():
+    """Pobierz nazwę katalogu od użytkownika."""
+    nazwa_katalogu = str(input("Podaj nazwę katalogu (lub zostaw puste dla domyślnej nazwy): ")).strip()
+    # Użyj domyślnej nazwy katalogu jeśli użytkownik nie podał nazwy
+    if not nazwa_katalogu:
+        teraz = datetime.now()
+        data_czas = teraz.strftime("%Y-%m-%d_%H-%M-%S")
+        nazwa_katalogu = f'pobrane_filmy_{data_czas}'
+    return nazwa_katalogu
 
-    # pobranie nazwy folderu od użytkownika
-    folder_name = str(input("Podaj nazwę folderu (lub zostaw puste dla domyślnej nazwy): ")).strip()
-
-    # użycie domyślnej nazwy folderu, jeśli użytkownik nie podał nazwy
-    if not folder_name:
-        now = datetime.now()
-        date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
-        folder_name = f'pobrane_filmy_{date_time}'
-
-    # pobranie każdego filmu po kolei
-    threads = []
-    for link in links:
-        t = threading.Thread(target=download_link, args=(link, folder_name))
+def pobierz_filmy(linki, nazwa_katalogu):
+    """Pobierz każdy film po kolei."""
+    watki = []
+    for link in linki:
+        t = threading.Thread(target=pobierz_film, args=(link, nazwa_katalogu))
         t.start()
-        threads.append(t)
-
-    # oczekiwanie na zakończenie wszystkich wątków
-    for t in threads:
+        watki.append(t)
+    # Poczekaj na zakończenie wszystkich wątków
+    for t in watki:
         t.join()
+
+if __name__ == '__main__':
+    pobierz_ytdlp()
+    
+    link_uzytkownika = pobierz_link_uzytkownika()
+    
+    zawartosc_strony = pobierz_zawartosc_strony(link_uzytkownika)
+    
+    linki = pobierz_linki_do_filmow(zawartosc_strony)
+    
+    nazwa_katalogu = pobierz_nazwe_katalogu()
+    
+    if utworz_katalog(nazwa_katalogu):
+        pobierz_filmy(linki, nazwa_katalogu)
